54f62d5d82e521c341ca3eef674f51437ea37118
Van Le <singularity@vannio.co.uk>
2016-06-26 22:21:01 +0100

    Renamed predicate method with all important question mark

--- app/server.rb
+++ app/server.rb
@@ -14,7 +14,7 @@ class Chitter < Sinatra::Base
       @current_user ||= User.get(session[:user_id])
     end
 
-    def can_reply_to_peep(peep)
+    def can_reply_to_peep?(peep)
       current_user && current_user != peep.user
     end
 
